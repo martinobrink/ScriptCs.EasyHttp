6fac1f8598161cdd3c9567faef574dbf54e0d350
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-01-23 21:39:00 +0100

    initial version 0.2.0 of ScriptCs.EasyHttp

---
+++ ScriptCs.EasyHttp/EasyHttp.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.EasyHttp
+{
+    public class EasyHttp : IScriptPackContext
+    {
+        public string Get(string url)
+        {
+            var httpClient = new HttpClient();
+            return httpClient.GetStringAsync(url).Result;
+        }
+
+        public HttpResponseMessage GetJson(string url)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient.GetAsync(url).Result;
+        }
+
+        public HttpResponseMessage PostJson<T>(string url, T objectToPost) where T : class
+        {
+            var httpClient = new HttpClient();
+            var bodyContent = new StringContent(JsonConvert.SerializeObject(objectToPost), Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient.PostAsync(url, bodyContent).Result;
+        }
+    }
+}
---
+++ ScriptCs.EasyHttp/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScriptCs.EasyHttp")]
+[assembly: AssemblyDescription("A ScriptCs script pack for easily creating GET/POST/PUT http requests and automatically serializing body json using Json.Net.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("@martinobrink")]
+[assembly: AssemblyProduct("ScriptCs.EasyHttp")]
+[assembly: AssemblyCopyright("Copyright © @martinobrink")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69341200-0230-8734-9F20-CE945B17013E")]
+
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
