62aac78301a3704e12e0a47c0a053e9ecc008a6b
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-01-23 23:53:55 +0100

    minor changes to nuspec file

--- ScriptCs.EasyHttp/Properties/AssemblyInfo.cs
+++ ScriptCs.EasyHttp/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("69341200-0230-8734-9F20-CE945B17013E")]
 
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.2")]
+[assembly: AssemblyFileVersion("0.3.2")]
+[assembly: AssemblyInformationalVersion("0.3.2")]
