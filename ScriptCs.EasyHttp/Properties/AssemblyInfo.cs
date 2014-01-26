219202bad27337c987ea3e74b7fa4f3e10621c06
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-01-26 19:35:10 +0100

    minor nuspec updates for better display/reference to githut repo etc.

--- ScriptCs.EasyHttp/Properties/AssemblyInfo.cs
+++ ScriptCs.EasyHttp/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ScriptCs.EasyHttp")]
-[assembly: AssemblyDescription("A ScriptCs script pack for easily creating GET/POST/PUT http requests and automatically serializing body json using Json.Net.")]
+[assembly: AssemblyDescription("A ScriptCs script pack for easily creating GET/POST/PUT/DELETE http requests and automatically serializing body json using Json.Net.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("@martinobrink")]
 [assembly: AssemblyProduct("ScriptCs.EasyHttp")]
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("69341200-0230-8734-9F20-CE945B17013E")]
 
-[assembly: AssemblyVersion("0.3.2")]
-[assembly: AssemblyFileVersion("0.3.2")]
-[assembly: AssemblyInformationalVersion("0.3.2")]
+[assembly: AssemblyVersion("0.3.4")]
+[assembly: AssemblyFileVersion("0.3.4")]
+[assembly: AssemblyInformationalVersion("0.3.4")]
